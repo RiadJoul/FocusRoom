f12612b6860ca94353094964b8d05e00414983f5
RIAD <riadjoul@gmail.com>
2025-12-03 03:54:12 +0100

    new link

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const websiteUrl = 'https://focusroomapp.vercel.app';
+export const websiteUrl = 'https://useFocusRoom.app';
 export const termsOfServiceUrl = `${websiteUrl}/terms-and-conditions`;
 export const privacyPolicyUrl = `${websiteUrl}/privacy-policy`;
 export const supportEmail = "riadjoul@gmail.com"
